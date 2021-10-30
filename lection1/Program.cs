728fe763bc48cececaad983f9f6577f95a9e148c
Rudenko Anastasiya <nzaveruha@list.ru>
2021-10-30 18:42:00 +0300

    prob 1

---
+++ lection1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Console.Clear();
+
+int xa = 20, ya = 1,
+    xb = 1, yb = 30,
+    xc = 40, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = xb;
+
+int count = 10;
+
